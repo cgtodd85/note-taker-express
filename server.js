52a2c65cd22353556c4396ef2fbf8be0fc27336c
Connor Todd <cgtodd85@gmail.com>
2021-07-23 16:49:06 -0400

    set default route to send the index html main page

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const db = require("./db/db.json");
 const PORT = 3001;
 
 //routes
 app.get("/", (req, res) => {
-  res.send("server is working!");
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", (req, res) => res.json(db));
