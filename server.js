1604295f9bb4ac10d18c84b7c7a7b57af503d868
Connor Todd <cgtodd85@gmail.com>
2021-07-23 16:03:51 -0400

    updated server route to point to db

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-
+const db = require("./db/db.json");
 const PORT = 3001;
 
 //routes
 app.get("/", (req, res) => {
   res.send("server is working!");
 });
+
+app.get("/notes", (req, res) => res.json(db));
 
 app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
