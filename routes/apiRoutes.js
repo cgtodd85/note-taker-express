ce26ddecbcd2fd6aab6a7425c435768da3c346f8
Connor Todd <cgtodd85@gmail.com>
2021-07-27 19:47:55 -0400

    re-did server.js in how it uses the routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const uuid = require("uuid");
+const uuid = require("uuid/v1");
 const router = express.router();
-const dbPath = require("../data/db.json");
+const dbPath = path.join(__dirname, "../data/db.json");
+
+//get
+
+//post
+
+//delete
--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
-
 const PORT = process.env.PORT || 3001;
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
+//enables app to receive and use urlencoded and json data payloads
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //using static middleware so html files have access to the css and js scripts
 app.use(express.static("public"));
-
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-require("./routes/apiRoutes")(app);
 //important to have path for api routes above the path for html routes
-require("./routes/htmlRoutes")(app);
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 //listener to 'start' ther server
 app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
