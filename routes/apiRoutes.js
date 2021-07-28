12b48e2631a955e7f1cae27bf122dce3edc7c355
Connor Todd <cgtodd85@gmail.com>
2021-07-28 10:41:17 -0400

    updating apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,8 +6,38 @@ const router = express.router();
 const dbPath = path.join(__dirname, "../data/db.json");
 
 //get
-router.get("/notes", (req, res) => fs.readFile(dbPath));
+router.get("/notes", (req, res) => {
+  fs.readFile(dbPath, (err, data) => {
+    if (err) {
+      return res.json(500);
+    } else {
+      let notes = JSON.parse(data);
+      return res.json(notes);
+    }
+  });
+});
 
 //post
 
 //delete
+
+/**
+ * potential ?
+ * 
+ * if (err) {
+    res.status(500);
+    return res.json();
+  } else {
+    fs.readFile(dbPath, (err, data) => {
+      
+    });
+    //
+
+    return res.json();
+  }
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
