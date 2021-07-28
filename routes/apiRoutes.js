d9b2c09117ff2955b4f4a62a00c11cd6afb37d95
Connor Todd <cgtodd85@gmail.com>
2021-07-28 17:43:28 -0400

    slight refactor of apiroutes, added removedattribute of readonly to render active note conditional

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,34 +3,40 @@ const fs = require("fs");
 const path = require("path");
 const { v1: uuidv1 } = require("uuid");
 const router = express.Router();
-const dbPath = path.join(__dirname, "../data/db.json");
+
+//path.join or require? require caches the result so path,join is better?
+//path.join assures separators are in the correct places
+//or just use file path alone as variable?
+// using this alone did not work: const dbFile = "../data/db.json";
+
+const dbFile = path.join(__dirname, "../data/db.json");
 
 //get
 router.get("/notes", (req, res) => {
-  fs.readFile(dbPath, (err, data) => {
+  fs.readFile(dbFile, "utf-8", (err, data) => {
     if (err) {
-      throw err;
+      console.log(err);
     } else {
       let notes = JSON.parse(data);
-      return res.json(notes);
+      res.json(notes);
     }
   });
 });
 
 //post
 router.post("/notes", (req, res) => {
-  fs.readFile(dbPath, (err, data) => {
+  fs.readFile(dbFile, "utf-8", (err, data) => {
     if (err) {
-      throw err;
+      console.log(err);
     } else {
-      let notes = JSON.parse(data);
-      let newId = uuidv1();
-      let newNote = { ...req.body, id: newId };
+      const notes = JSON.parse(data);
+      const newId = uuidv1();
+      const newNote = { ...req.body, id: newId };
       notes.push(newNote);
-      fs.writeFile(dbPath, JSON.stringify(notes), (err) =>
+      fs.writeFile(dbFile, JSON.stringify(notes, null, 2), (err) =>
         err ? console.log(err) : console.log("added new note!")
       );
-      return res.json(notes);
+      res.json(notes);
     }
   });
 });
@@ -38,19 +44,21 @@ router.post("/notes", (req, res) => {
 //delete
 router.delete("/notes/:id", (req, res) => {
   const noteId = req.params.id;
-  fs.readFile(dbPath, (err, data) => {
+  fs.readFile(dbFile, "utf-8", (err, data) => {
     if (err) {
-      throw err;
+      console.log(err);
     } else {
       const notes = JSON.parse(data);
       const filteredNotes = notes.filter((note) => note.id !== noteId);
-      fs.writeFile(dbPath, JSON.stringify(filteredNotes), (err) =>
+      fs.writeFile(dbFile, JSON.stringify(filteredNotes, null, 2), (err) =>
         err ? console.log(err) : console.log("deleted a note!")
       );
-      return res.json(notes);
+      res.json();
     }
   });
 });
+
+///// for post and delete.. res.end or return res json or res send
 
 ///////////successfully deleted notes but for some reason I can't go add a new note after this
 
